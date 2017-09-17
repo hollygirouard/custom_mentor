6e3b772a29713376ec6eda605e577ecfd4b7222d
austintedwards <austintedwards@gmail.com>
2017-09-16 20:57:20 -0600

    working will navigation

---
+++ src/components/page_content.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style/navigation.css'
+import { Nav, NavItem } from 'react-bootstrap';
+
+
+const PageContent = React.createClass({
+  render() {
+    return (
+    <div className ="col-sm-2">
+      Hello
+    </div>
+    );
+  }
+});
+
+
+export default PageContent
--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import NavList from './components/navigation'
+import PageContent from './components/page_content'
+
+class App extends Component {
+  constructor(props){
+  super(props);
+  this.state={page: [],
+  selectedPage:null
+  }
+}
+  render() {
+
+    return (
+      <div>
+        <NavList/>
+        <PageContent page={this.state.selectedPage}/>
+      </div>
+
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>, document.getElementById('root'));
