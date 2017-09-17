6a48c068b2f7989952ac71469c94dbad8a968d91
austintedwards <austintedwards@gmail.com>
2017-09-17 10:28:08 -0600

    contact information on

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -9,7 +9,11 @@ export class Contact extends Component {
     render() {
         return (
             <div>
-                <h3>Contact</h3>
+                <h3>Contact Blossom Care Network</h3>
+                <p>1600 Downing Street</p>
+                <p>Suite 240</p>
+                <p>Denver CO 80218</p>
+                <a href="tel:+13035782422">303.578.2422</a>
                 <button onClick={this.onNavigateHome} className="btn btn-primary">Go Home!</button>
             </div>
         );
