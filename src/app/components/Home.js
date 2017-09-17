24103068cb8f89d9dbdfce74c1a1a9fd560876f5
austintedwards <austintedwards@gmail.com>
2017-09-17 10:24:27 -0600

    menu working and logo in

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {Component} from "react";
 
-export class Home extends React.Component {
+export class Home extends Component {
     render() {
         return (
             <div>
--- src/app/components/Root.js
+++ src/app/components/Root.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
 
 import {Header} from "./Header";
 
-export class Root extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-2">
-                        <Header />
-                    </div>
-                    <div className="col-xs-10 ">
-                        {this.props.children}
-                    </div>
-                </div>
+export class Root extends Component {
+  facebook() {
+    window.open("https://www.facebook.com/blossomcarenetwork.org");
+  }
+  twitter() {
+    window.open("https://twitter.com/BlossomCareCO");
+  }
+  linkedin() {
+    window.open("https://www.linkedin.com/company/3800360/");
+  }
+  youtube() {
+    window.open("https://www.youtube.com/channel/UCA1mpGozH327Ca2NfSSYUEQ");
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-2">
+            <img className = "logo" src = "../image/CustomMentorLogo.png"/>
+            <Header/>
+            <div className ="socialLinks">
+              <i onClick={this.facebook} className="fa fa-facebook-official"></i>
+              <i onClick={this.twitter} className="fa fa-twitter-square"></i>
+              <i onClick={this.linkedin} className="fa fa-linkedin-square"></i>
+              <i onClick={this.youtube} className="fa fa-youtube-square"></i>
             </div>
-        );
-    }
+          </div>
+          <div className="col-xs-10 ">
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
