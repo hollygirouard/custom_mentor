4d887b3aa33ed8d84a62609188e942c6b29fc589
austintedwards <austintedwards@gmail.com>
2017-09-22 11:55:29 -0600

    working router

--- src/app/index.js
+++ src/app/index.js
@@ -3,13 +3,20 @@ import {render} from "react-dom";
 import {Router, Route, browserHistory, IndexRoute} from "react-router";
 
 import {Home} from "./components/Home";
+import {MentorPage} from "./components/MentorPage";
+import {MenteePage} from "./components/MenteePage";
+
 
 
 class App extends Component {
     render() {
         return (
-            <div><Home /></div>
+          <Router history={browserHistory}>
+              <Route path={"/"} component={Home} ></Route>
+              <Route path={"/mentor"} component={MentorPage} ></Route>
+              <Route path={"/mentee"} component={MenteePage} ></Route>
 
+          </Router>
         );
     }
 }
