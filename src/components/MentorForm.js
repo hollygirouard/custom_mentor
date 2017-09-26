dcaa324d317c1398d4863beddafaf75e97300784
austintedwards <austintedwards@gmail.com>
2017-09-26 16:04:33 -0600

    working with mentor/mentee form

--- src/components/MentorForm.js
+++ src/components/MentorForm.js
@@ -1,11 +1,50 @@
 import React, {Component} from "react";
 
 export class MentorForm extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Mentor Profile</h3>
-            </div>
-        );
-    }
+  constructor(props) {
+      super(props)
+      this.state = {
+          formValues: {}
+      }
+  }
+
+  handleChange(event) {
+      event.preventDefault();
+      let formValues = this.state.formValues;
+      let name = event.target.name;
+      let value = event.target.value;
+      formValues[name] = value;
+      this.setState({formValues})
+  }
+  handleSubmit(event) {
+      event.preventDefault();
+      this.props.onSignIn(this.state.formValues)
+  }
+
+  render() {
+    return (
+      <div>
+      <h2>Mentor Profile</h2>
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+      </div>
+    );
+  }
 }
