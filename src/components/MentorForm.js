35c88af1f44d96d8e91270339459fd54c1eb131d
austintedwards <austintedwards@gmail.com>
2017-09-26 16:27:51 -0600

    adding form

--- src/components/MentorForm.js
+++ src/components/MentorForm.js
@@ -2,48 +2,44 @@ import React, {Component} from "react";
 
 export class MentorForm extends Component {
   constructor(props) {
-      super(props)
-      this.state = {
-          formValues: {}
-      }
+    super(props)
+    this.state = {
+      formValues: {}
+    }
   }
 
   handleChange(event) {
-      event.preventDefault();
-      let formValues = this.state.formValues;
-      let name = event.target.name;
-      let value = event.target.value;
-      formValues[name] = value;
-      this.setState({formValues})
+    event.preventDefault();
+    let formValues = this.state.formValues;
+    let name = event.target.name;
+    let value = event.target.value;
+    formValues[name] = value;
+    this.setState({formValues})
   }
   handleSubmit(event) {
-      event.preventDefault();
-      this.props.onSignIn(this.state.formValues)
+    event.preventDefault();
+    console.log(this.state)
+
+    // this.props.onMentorSubmit(this.state.formValues)
   }
 
   render() {
     return (
       <div>
-      <h2>Mentor Profile</h2>
-      <form>
-        <label>
-          Is going:
-          <input
-            name="isGoing"
-            type="checkbox"
-            checked={this.state.isGoing}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Number of guests:
-          <input
-            name="numberOfGuests"
-            type="number"
-            value={this.state.numberOfGuests}
-            onChange={this.handleInputChange} />
-        </label>
-      </form>
+        <h2>Mentor Profile</h2>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <label>
+            <input name="educational" type="checkbox" value={this.state.formValues["educational"]} onChange={this.handleChange.bind(this)}/>
+            Educational
+          </label>
+          <br/>
+          <label>
+            <input name="financial" type="checkbox" value={this.state.formValues["financial"]} onChange={this.handleChange.bind(this)}/>
+            Financial
+          </label>
+          <br/><input className="btn btn-primary" type="submit" value="Submit" />
+
+        </form>
       </div>
     );
   }
