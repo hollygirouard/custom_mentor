822fb7cbc121129fb77c08c3faf226122416b86d
austintedwards <austintedwards@gmail.com>
2017-09-27 13:21:23 -0600

    working on form

--- src/components/Mentor/MentorForm.js
+++ src/components/Mentor/MentorForm.js
@@ -4,7 +4,14 @@ export class MentorForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      formValues: {}
+      formValues: {
+        educational: false,
+        financial: false,
+        physical: false,
+        spiritual: false,
+        other: false,
+        helpInfo: ""
+      }
     }
   }
 
@@ -14,6 +21,14 @@ export class MentorForm extends Component {
     let name = event.target.name;
     let value = event.target.value;
     formValues[name] = value;
+    this.setState({formValues})
+  }
+  handleCheckedChange(event) {
+    event.preventDefault();
+    let formValues = this.state.formValues;
+    let name = event.target.name;
+    let checked = event.target.checked;
+    formValues[name] = checked;
     this.setState({formValues})
   }
   handleSubmit(event) {
@@ -29,15 +44,40 @@ export class MentorForm extends Component {
         <h2>Mentor Profile</h2>
         <form onSubmit={this.handleSubmit.bind(this)}>
           <label>
-            <input name="educational" type="checkbox" value={this.state.formValues["educational"]} onChange={this.handleChange.bind(this)}/>
+            <input name="educational" type="checkbox" checked={this.state.formValues["educational"]} onChange={this.handleCheckedChange.bind(this)}/>
             Educational
           </label>
           <br/>
           <label>
-            <input name="financial" type="checkbox" value={this.state.formValues["financial"]} onChange={this.handleChange.bind(this)}/>
+            <input name="financial" type="checkbox" checked={this.state.formValues["financial"]} onChange={this.handleCheckedChange.bind(this)}/>
             Financial
           </label>
-          <br/><input className="btn btn-primary" type="submit" value="Submit" />
+          <br/>
+          <label>
+            <input name="physical" type="checkbox" checked={this.state.formValues["physical"]} onChange={this.handleCheckedChange.bind(this)}/>
+            Physical (health)
+          </label>
+          <br/>
+          <label>
+            <input name="spiritual" type="checkbox" checked={this.state.formValues["spiritual"]} onChange={this.handleCheckedChange.bind(this)}/>
+            Spiritual
+          </label>
+          <br/>
+          <label>
+            <input name="other" type="checkbox" checked={this.state.formValues["other"]} onChange={this.handleCheckedChange.bind(this)}/>
+            Other
+          </label>
+          <br/>
+          <label>
+            Explain how you can help:
+          </label>
+          <br/>
+          <textarea style={{
+            width: '50%',
+            height: '200%'
+          }} name="helpInfo" value={this.state.formValues["helpInfo"]} onChange={this.handleChange.bind(this)}/>
+
+          <br/><input className="btn btn-primary" type="submit" value="Submit"/>
 
         </form>
       </div>
