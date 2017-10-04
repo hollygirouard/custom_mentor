cd19f676f0790c3d55c84d65b1c9e790d20b50d9
austintedwards <austintedwards@gmail.com>
2017-10-04 13:39:50 -0600

    removing from api in same file

--- src/components/Home/Signup.js
+++ src/components/Home/Signup.js
@@ -56,7 +56,7 @@ export class Signup extends Component {
       console.log("sending data")
       axios({
         method: 'POST',
-        url: 'custom_mentor/serverapi/user.php',
+        url: 'http://localhost:8050/custom_mentor/serverapi/user.php',
         data: "requesttype=Signup&data=" + (JSON.stringify(form))
       }).then(function(response) {
         //sample response :{"response":"failed","error":"Your email has been registered. Please pick another email.",type:""}
