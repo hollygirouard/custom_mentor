a4d4dea6b4c4a25fee40bebcdf4c80a0a76bd98e
kelvin sajere <kelvinsajere@github.com>
2017-10-09 18:57:40 -0600

    Update Data Base to accomdate for foreign Key

--- database/custom_mentor.sql
+++ database/custom_mentor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2017 at 12:30 AM
+-- Generation Time: Oct 10, 2017 at 12:56 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -23,6 +23,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `profile_id` int(11) NOT NULL,
+  `fk_id` int(11) NOT NULL,
+  `goals` varchar(255) DEFAULT NULL,
+  `service` varchar(255) DEFAULT NULL,
+  `mentoring_level` varchar(255) DEFAULT NULL,
+  `weektalk` varchar(255) DEFAULT NULL,
+  `contact` varchar(255) DEFAULT NULL,
+  `avialability` varchar(255) DEFAULT NULL,
+  `areaofexp` varchar(255) DEFAULT NULL,
+  `experience` varchar(255) DEFAULT NULL,
+  `fieldofstudy` varchar(255) DEFAULT NULL,
+  `education` varchar(255) DEFAULT NULL,
+  `managementool` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`profile_id`, `fk_id`, `goals`, `service`, `mentoring_level`, `weektalk`, `contact`, `avialability`, `areaofexp`, `experience`, `fieldofstudy`, `education`, `managementool`) VALUES
+(1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,20 +63,26 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `type`, `phone`) VALUES
-(1, 'Test1', 'test1@test1.com', '$2y$10$DAbhUFoKXjK7Qt.hxD5pFOULVEYbuL9p325/DD7vmnC33sC/C.Y4a', 'Mentee', '00000'),
-(2, 'Test2', 'test2@test2.com', '$2y$10$9PpWAshrTbx3atK86jgqOO59TB64k6djUSJEAmhhwiceU7/yjz7Gu', 'Mentee', '00000'),
-(3, 'demo', 'demo@demo.com', '$2y$10$xz./HsIFYf7jK/6FjO9mneh1BthBd6S5vPEoxGdF76zcQiACMpl9q', 'Both', '00000');
+(1, 'demo', 'demo@demo.com', '$2y$10$7/c79SOsg6OoDtDY5j710uulAZQADsoJwDwZ7VvnheBt8iYCEJ4hS', 'Mentee', '123123'),
+(2, 'demo2', 'demo2@demo.com', '$2y$10$1.aD/ecWVWBTO2JV0bb9i.r8zkvUViq0AWW83SqE32lITJjs5H9qi', 'Mentee', '123123');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profile_id`),
+  ADD KEY `fk_id` (`fk_id`);
 
 --
 -- Indexes for table `users`
@@ -59,10 +95,25 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`fk_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
