084be9a76b01c8e7c48800c1d6753882193326bf
austintedwards <austintedwards@gmail.com>
2017-10-19 16:00:13 -0600

    fixing database route

--- serverapi/config/database.php
+++ serverapi/config/database.php
@@ -1,26 +1,21 @@
 <?php
 //session_start();
 class Database{
-
     // specify your own database credentials
-    private $host = "http://custommentor-env.mbkr62zap6.us-east-2.elasticbeanstalk.com/";
+    private $host = "localhost";
     private $db_name = "custom_mentor";
-    private $username = "custommentor";
-    private $password = "custommentor";
+    private $username = "root";
+    private $password = "";
     public $conn;
-
     // get the database connection
     function __construct() {
-
         $this->conn = null;
-
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
-
         //return $this->conn;
     }
 }
