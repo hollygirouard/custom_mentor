8ab264166f53e870815500f3b34e41fbbb50cdc0
kelvin sajere <kelvinsajere@github.com>
2017-10-30 03:03:59 -0600

    dev and Prod Environment

--- serverapi/config/database.php
+++ serverapi/config/database.php
@@ -9,11 +9,11 @@ class Database{
     public function __construct() {
 
         $this->conn = null;
-		$this->host = $_SERVER['RDS_HOSTNAME'];
-		$this->db_name = $_SERVER['RDS_DB_NAME'];
-		$this->username = $_SERVER['RDS_USERNAME'];
-		$this->password = $_SERVER['RDS_PASSWORD'];
-		$this->port = $_SERVER['RDS_PORT'];
+		$this->host = isset($_SERVER['RDS_HOSTNAME'])?$_SERVER['RDS_HOSTNAME']:"localhost";
+		$this->db_name = isset($_SERVER['RDS_DB_NAME']) ? $_SERVER['RDS_DB_NAME'] :"custom_mentor";
+		$this->username = isset($_SERVER['RDS_USERNAME']) ? $_SERVER['RDS_USERNAME'] :"root";
+		$this->password = isset($_SERVER['RDS_PASSWORD']) ?$_SERVER['RDS_PASSWORD'] :"";
+		$this->port = isset($_SERVER['RDS_PORT']) ? :"8080";
 
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
