c8edc8134fed8028cfd4ad333ddbeea2a4ab3d7d
Ryan <ryan.d.elliott@gmail.com>
2017-10-29 20:51:42 -0600

    Move embeded scripts from composer into js file

---
+++ blog-component/public/composer.js
@@ -0,0 +1,56 @@
+
+/*************************
+ *Quill Text Editor Setup*
+ *************************/
+
+/* toolbar options */
+var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+    ['blockquote', 'code-block'],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
+
+    [{ 'size': ['small', false, 'large', 'huge'] }], // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }], // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+    ['clean'],
+    ['link','image'] // remove formatting button
+];
+
+/* Initialize quill in #editor div */
+var quill = new Quill('#editor', {
+    modules: {
+        toolbar: toolbarOptions
+    },
+    theme: 'snow'
+});
+
+
+/*************************
+ *Handle Blog Post Submit*
+ *************************/
+
+$('form').on('submit', (event) => {
+    event.preventDefault();
+    $.ajax({
+        url: '/',
+        type: 'POST',
+        data: {content: $('.ql-editor').html()},
+    })
+    .done(function(data) {
+        console.log("success");
+        console.log(data);
+        window.location.href = '/';
+    })
+    .fail(function(err) {
+        console.log(err);
+        $('.alertcontainer').html($('<div>').addClass('alert alert-danger').text(err.statusText));
+        $('body').scrollTop(0);
+    })
+    .always(function() {
+        console.log("complete");
+    });
+});
