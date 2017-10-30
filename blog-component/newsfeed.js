0fbff3ab34ed86363fe7967a3c442dc4e64935f3
miketheo423 <miketheo423@gmail.com>
2017-10-29 18:45:44 -0600

    Adds search bar functionality and appends links to page

--- blog-component/newsfeed.js
+++ blog-component/newsfeed.js
@@ -1,9 +1,19 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-
-
-
-
-
+	// Prevents enter key from refreshing the page
+	$('#search-bar').submit(function() {
+		event.preventDefault();
+		let searchQuery = $('input[name="query"]').val();
+		console.log(searchQuery);
+		let URL = "https://www.reddit.com/r/" + searchQuery + ".json";
+		$.get(URL).done(function(data) {
+			let newsData = data.data.children;
+			console.log(newsData);
+			for (let i = 0; i < newsData.length; i++) {
+				$('#news-feed').append(('<div class="card">'+ '<a href="' + newsData[i].data.url + '">' + newsData[i].data.title + '</a>' + '</div>'));
+			}
+		});
+	});
+	
 });
