393e1b01b63853dda5cbf8b66cf67b82ebdcf660
miketheo423 <miketheo423@gmail.com>
2017-10-30 11:14:22 -0600

    Adds default news stories on page load

--- blog-component/public/newsfeed.js
+++ blog-component/public/newsfeed.js
@@ -2,6 +2,7 @@ const apiKey = "115c5979-da30-4257-b1ec-b3f0156feffa";
 
 $( document ).ready(function() {
     console.log("ready!");
+    mostRecent();
 
   // Global Variables 
     let page = 1;
@@ -12,13 +13,13 @@ $( document ).ready(function() {
 		event.preventDefault();
 		$('#news-feed').empty();
 		guardianCall();
-	});
 
-	// Loads more articles as the user scrolls down
-	$(window).scroll(function() {   
-		if($(window).scrollTop() + $(window).height() == $(document).height()) {
-			guardianCall();
-		}
+		// Loads more articles as the user scrolls down
+		$(window).scroll(function() {   
+			if($(window).scrollTop() + $(window).height() == $(document).height()) {
+				guardianCall();
+			}
+		});
 	});
 
 	// Ajax call for first page of subreddit results
@@ -30,10 +31,25 @@ $( document ).ready(function() {
 			let newsData = data.response.results;
 			console.log(newsData);
 			for (let i = 0; i < newsData.length; i++) {
-				$('#news-feed').append(('<div class="card">'+ '<a href="' + newsData[i].webUrl + '" target="_blank">' + '<h3 class="card-header">' + newsData[i].webTitle + '</h3>' + '</a>' + '<h5 class="news-section">' + "Section: " + newsData[i].sectionName + '</h5>' + '</div>'));
-				$('.card').append();
+				$('#news-feed').append(('<div class="card">'+ '<a href="' + newsData[i].webUrl + '" target="_blank">' + 
+					'<h3 class="card-header">' + newsData[i].webTitle + '</h3>' + '</a>' + 
+					'<h5 class="news-section">' + "Section: " + newsData[i].sectionName + '</h5>' + '</div>'));
 			}
 		});
 		page++;
 	}
+
+	  // Loads most recent articles as default
+  function mostRecent() {
+ 		 let URL = "http://content.guardianapis.com/search?q&page-size=200&order-by=newest&api-key=115c5979-da30-4257-b1ec-b3f0156feffa";
+ 		 $.get(URL).done(function(data) {
+ 		 		let newsData = data.response.results;
+ 		 		for (let i = 0; i < newsData.length; i++) {
+				$('#news-feed').append(('<div class="card">'+ '<a href="' + newsData[i].webUrl + '" target="_blank">' + 
+					'<h3 class="card-header">' + newsData[i].webTitle + '</h3>' + '</a>' + 
+					'<h5 class="news-section">' + "Section: " + newsData[i].sectionName + '</h5>' + '</div>'));
+			}
+ 		 });
+	}
+
 });
