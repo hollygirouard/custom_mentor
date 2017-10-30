04ba2590f733ae84fe266f25b5750c39a23dfb9a
Kelrynn <cameroncastells@gmail.com>
2017-10-30 09:00:12 -0600

    fixing newsfeed.js file location issues

--- blog-component/public/newsfeed.js
+++ blog-component/public/newsfeed.js
@@ -1,19 +1,38 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log("ready!");
 
-	// Prevents enter key from refreshing the page
+  // Global Variables 
+    let count = 25;
+    let after;
+
+	// On submit of search bar ajax call is make
 	$('#search-bar').submit(function() {
+		count = 25;
+		after = "";
 		event.preventDefault();
+		$('#news-feed').empty();
+		redditCall();
+	});
+
+	// Loads more articles as the user scrolls down
+	$(window).scroll(function() {   
+		if($(window).scrollTop() + $(window).height() == $(document).height()) {
+			redditCall();
+		}
+	});
+
+	// Ajax call for first page of subreddit results
+	function redditCall() {
 		let searchQuery = $('input[name="query"]').val();
-		console.log(searchQuery);
-		let URL = "https://www.reddit.com/r/" + searchQuery + ".json";
+		let URL = "https://www.reddit.com/r/" + searchQuery + ".json?count=" + count + "&after=" + after;
 		$.get(URL).done(function(data) {
+			console.log(URL);
 			let newsData = data.data.children;
-			console.log(newsData);
+			after = data.data.after;
 			for (let i = 0; i < newsData.length; i++) {
-				$('#news-feed').append(('<div class="card">'+ '<a href="' + newsData[i].data.url + '">' + newsData[i].data.title + '</a>' + '</div>'));
+				$('#news-feed').append(('<div class="card">'+ '<a href="' + newsData[i].data.url + '" target="_blank">' + newsData[i].data.title + '</a>' + '</div>'));
 			}
 		});
-	});
-	
+		count+=25;
+	}
 });
