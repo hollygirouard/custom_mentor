4d2f6819c032009babd90f0ebade8791d9a50abe
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-31 17:22:08 -0600

    Fix linter errors in MentorInfo

--- src/components/Home/MentorInfo.js
+++ src/components/Home/MentorInfo.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
+/* eslint-disable max-len */
 export default class MentorInfo extends Component {
   render() {
     return (
       <div>
         <h3>Why should I be a CustomMentor?</h3>
         <p>Almost everyone can be a mentor. With any background whatsoever, whether a student,
-businessperson, doctor, scientist, waiter, checkout clerk, convict, pastor or unemployed person, you
-have skills, experience and insight to pass along.
+          businessperson, doctor, scientist, waiter, checkout clerk, convict, pastor or unemployed person, you
+          have skills, experience and insight to pass along.
         </p>
         <p>As independent contractors, CustomMentor allows its mentors to choose when and how much to work,
-what to charge and who to mentor. Guided by a comprehensive profile CustomMentors are matched
-with likely mentees based on your specifications. People will pay for your mentoring services based on
-what you bring to the table; your personality, education, intelligence, experience and potential for
-contribution.
+          what to charge and who to mentor. Guided by a comprehensive profile CustomMentors are matched
+          with likely mentees based on your specifications. People will pay for your mentoring services based on
+          what you bring to the table; your personality, education, intelligence, experience and potential for
+          contribution.
         </p>
         <p>The benefits for CustomMentors are…</p>
         <ol>
@@ -23,8 +24,8 @@ contribution.
           <li>Engagement – dealing with a variety of mentees keeps work fresh and interesting</li>
           <li>Feedback – frequent opportunities for comment and a built-in ratings system</li>
           <li>Competitive pay – CustomMentors charge what they want based on what they can offer and
-what the market will bear. Mentees decide if they want to pay the asking price, negotiate, or
-find a different mentor.
+            what the market will bear. Mentees decide if they want to pay the asking price, negotiate, or
+            find a different mentor.
           </li>
         </ol>
         <p>Beyond these, there are many other benefits to mentoring someone, including…</p>
@@ -38,7 +39,6 @@ find a different mentor.
           <li>opportunity to change another’s life</li>
           <li>positive self-esteem from helping another</li>
         </ol>
-
       </div>
     );
   }
