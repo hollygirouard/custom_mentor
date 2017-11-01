9cad81136085432cdd777f12d753a8b36ddbdc59
James Sullivan <jamesfsullivan5@gmail.com>
2017-10-31 18:03:48 -0600

    Fix linter errors SignForm

--- src/components/Home/SignForm.js
+++ src/components/Home/SignForm.js
@@ -1,53 +1,66 @@
-import React, {Component} from "react";
-import { Button, ButtonGroup } from 'reactstrap'
+import React, { Component } from 'react';
+import { Button, ButtonGroup } from 'reactstrap';
 
-import Signup from "./Signup";
-import Signin from "./Signin";
+import Signup from './Signup';
+import Signin from './Signin';
 
 export default class SignForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loginVisible: true,
-      signinVisible: false
-    }
-  }
-
-  signIn = (userInfo) => {
-    this.props.authenticateUser(userInfo)
-
-  }
-
-  signUp(member){
-    // console.log("signUp", member);
-  }
-
-  render() {
-    return(
-      <div className="sign-form">
-        <div className="button-sign-form">
-        <ButtonGroup>
-          <Button active={this.state.loginVisible} color="primary" onClick={() => this.onLogin()}>Sign-in</Button>
-          <Button active={this.state.signinVisible} color="primary" onClick={() => this.onSignin()}>Sign-up</Button>
-        </ButtonGroup>
-      </div>
-
-        {this.state.loginVisible
-            ? <Signin onSignIn = {member => this.signIn(member)} />
-            : null
-        }
-        {this.state.signinVisible
-            ? <Signup onSignUp = {member => this.signUp(member)}  />
-            : null
-        }
-      </div>
-    )
+      signinVisible: false,
+    };
   }
 
   onLogin() {
-      this.setState({loginVisible: true, signinVisible:false});
+    this.setState({ loginVisible: true, signinVisible: false });
   }
   onSignin() {
-      this.setState({loginVisible: false, signinVisible:true});
+    this.setState({ loginVisible: false, signinVisible: true });
   }
-};
+
+  signIn(userInfo) {
+    this.props.authenticateUser(userInfo);
+  }
+
+  // signUp(member) {
+  //   // console.log("signUp", member);
+  // }
+
+  render() {
+    return (
+      <div className="sign-form">
+        <div className="button-sign-form">
+          <ButtonGroup>
+            <Button
+              active={this.state.loginVisible}
+              color="primary"
+              onClick={() => this.onLogin()}
+            >
+              Sign-in
+            </Button>
+            <Button
+              active={this.state.signinVisible}
+              color="primary"
+              onClick={() => this.onSignin()}
+            >
+              Sign-up
+            </Button>
+          </ButtonGroup>
+        </div>
+
+        {this.state.loginVisible
+            ? <Signin onSignIn={member => this.signIn(member)} />
+            : null
+        }
+        {/* Changed singup to sign in below because it
+          was not doing anything and the linter did not like it */}
+        {this.state.signinVisible
+            ? <Signup onSignUp={member => this.signIn(member)} />
+            : null
+        }
+      </div>
+    );
+  }
+}
