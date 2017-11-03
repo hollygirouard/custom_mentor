986521216d88861cc2ada33ac19ab9f2bc8de1be
Kara Fox <fox.kara@gmail.com>
2017-11-03 11:00:44 -0600

    Cleaned up the rest of the home page styling/theming

--- src/components/Home/MenteeInfo.js
+++ src/components/Home/MenteeInfo.js
@@ -9,14 +9,14 @@ export default class MenteeInfo extends Component {
             <div className="row">
               <div className="header-section text-center">
                 <h2>Why do I need a mentor?</h2>
-                <p>CustomMentor maintains that absolutely everyone needs a mentor. If honest, anyone that has achieved success will credit to one extent or another a mentor (or perhaps more than one) in their lives. Regardless of the goal, a mentor can help you get there.</p>
+                <p>CustomMentor maintains that absolutely everyone needs a mentor. If honest, anyone that has achieved success will credit to one extent or another a mentor (or perhaps more than one) in their lives. Regardless of your goals, a mentor can help you get there.</p>
                 <hr className="bottom-line" />
               </div>
               <div className="feature-info">
                 <div className="fea">
                   <div className="col-md-4">
                     <div className="heading pull-right">
-                      <h4>Achieve More!</h4>
+                      <h4>Achieve More</h4>
                       <p>Whether it is saving for a vacation or retirement, losing weight or getting fit, starting a business or getting a promotion, passing a course or finding your life’s purpose, and anything else, a good mentor is essential.</p>
                     </div>
                     <div className="fea-img pull-left">
@@ -27,7 +27,7 @@ export default class MenteeInfo extends Component {
                 <div className="fea">
                   <div className="col-md-4">
                     <div className="heading pull-right">
-                      <h4>Personalized Experience!</h4>
+                      <h4>Personalized Experience</h4>
                       <p>Specific personalities and needs of every person vary, so we tailor to your exact specifications.
                         We create a secure atmosphere and match for your personalized profile.</p>
                     </div>
@@ -40,18 +40,16 @@ export default class MenteeInfo extends Component {
                   <div className="col-md-4">
                     <div className="heading pull-right">
                       <h4>Succed!</h4>
-                      <ol>
-                        <li>Increased confidence</li>
-                        <li>Access to role models</li>
-                        <li>Higher probability of goal attainment</li>
-                        <li>Personal links to valuable resources &amp; skill sets</li>
-                        <li>Support in attaining skills/ knowledge/ attitudes/ culture awareness</li>
-                        <li>Encouragement for faster learning and growth—both personal and professional</li>
-                        <li>Greater exposure and visibility</li>
-                        <li>Increased feeling of being more valued as an employee</li>
-                        <li>Improved communication and expanded network</li>
-                        <li>130% greater likelihood of being in a leadership role</li>
-                      </ol>
+                      <p>Increased confidence</p>
+                      <p>Access to role models</p>
+                      <p>130% greater likelihood of being in a leadership role</p>
+                      <p>Bring more value as an employee</p>
+                      <p>Improved communication and expanded network</p>
+                      <p>Greater exposure and visibility</p>
+                      <p>Access to valuable resources</p>
+                      {/* <p>Higher probability of goal attainment</p> */}
+                      {/* <p>Support in attaining skills/ knowledge/ attitudes/ culture awareness</p> */}
+                      {/* <p>Encouragement for faster learning and growth—both personal and professional</p> */}
                     </div>
                     <div className="fea-img pull-left">
                       <i className="fa fa-trophy"></i>
--- src/components/Home/MentorInfo.js
+++ src/components/Home/MentorInfo.js
@@ -3,43 +3,55 @@ import React, { Component } from 'react';
 export default class MentorInfo extends Component {
   render() {
     return (
-      <div>
-        <h3>Why should I be a CustomMentor?</h3>
-        <p>Almost everyone can be a mentor. With any background whatsoever, whether a student,
-businessperson, doctor, scientist, waiter, checkout clerk, convict, pastor or unemployed person, you
-have skills, experience and insight to pass along.
-        </p>
-        <p>As independent contractors, CustomMentor allows its mentors to choose when and how much to work,
-what to charge and who to mentor. Guided by a comprehensive profile CustomMentors are matched
-with likely mentees based on your specifications. People will pay for your mentoring services based on
-what you bring to the table; your personality, education, intelligence, experience and potential for
-contribution.
-        </p>
-        <p>The benefits for CustomMentors are…</p>
-        <ol>
-          <li>Flexibility – choose what type of mentoring to provide (cheerleader, mentor, coach, tutor)</li>
-          <li>Convenient – work when, where, and how much you want</li>
-          <li>Work with people you like – CustomMentors choose which mentees they want to work with</li>
-          <li>Engagement – dealing with a variety of mentees keeps work fresh and interesting</li>
-          <li>Feedback – frequent opportunities for comment and a built-in ratings system</li>
-          <li>Competitive pay – CustomMentors charge what they want based on what they can offer and
-what the market will bear. Mentees decide if they want to pay the asking price, negotiate, or
-find a different mentor.
-          </li>
-        </ol>
-        <p>Beyond these, there are many other benefits to mentoring someone, including…</p>
-        <ol>
-          <li>learning from your mentee</li>
-          <li>expansion of sphere of influence</li>
-          <li>reinforcement of accomplishments</li>
-          <li>opportunity for reciprocation</li>
-          <li>increased recognition</li>
-          <li>improved communication and people skills</li>
-          <li>opportunity to change another’s life</li>
-          <li>positive self-esteem from helping another</li>
-        </ol>
-
-      </div>
+      <section className="section-padding dark-section">
+        <div className="container">
+          <div className="row">
+            <div className="header-section text-center">
+              <h2 className="white">Why should I be a CustomMentor?</h2>
+              <p className="white">Almost everyone can be a mentor. With any background whatsoever, whether a student,
+    businessperson, doctor, scientist, waiter, checkout clerk, convict, pastor or unemployed person, you
+    have skills, experience and insight to pass along.</p>
+              <p className="white">As independent contractors, CustomMentor allows its mentors to choose when and how much to work,
+    what to charge and who to mentor. Guided by a comprehensive profile CustomMentors are matched
+    with likely mentees based on your specifications. People will pay for your mentoring services based on
+    what you bring to the table; your personality, education, intelligence, experience and potential for
+    contribution.</p>
+              <hr className="bottom-line bg-white" />
+            </div>
+            <div className="col-md-6 col-sm-6">
+              <div className="text-comment">
+                <h3>Exclusive CustomMentor Benefits</h3>
+                <ol className="text-par">
+                  <li><strong>Flexibility</strong> &mdash; choose what type of mentoring to provide (cheerleader, mentor, coach, tutor)</li>
+                  <li><strong>Convenient</strong> &mdash; work when, where, and how much you want</li>
+                  <li><strong>Work with people you like</strong> &mdash; CustomMentors choose which mentees they want to work with</li>
+                  <li><strong>Engagement</strong> &mdash; dealing with a variety of mentees keeps work fresh and interesting</li>
+                  <li><strong>Feedback</strong> &mdash; frequent opportunities for comment and a built-in ratings system</li>
+                  <li><strong>Competitive pay</strong> &mdash; CustomMentors charge what they want based on what they can offer and
+        what the market will bear. Mentees decide if they want to pay the asking price, negotiate, or
+        find a different mentor.
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-6">
+              <div className="text-comment">
+                <h3>Additional Mentor Benefits</h3>
+                <ol className="text-par">
+                  <li>Expand your of sphere of influence</li>
+                  <li>Learn from your mentee</li>
+                  <li>Reinforce accomplishments</li>
+                  <li>Opportunity for reciprocation</li>
+                  <li>Increased recognition</li>
+                  <li>Improved communication and people skills</li>
+                  <li>Opportunity to change another’s life</li>
+                  <li>Positive self-esteem from helping another</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
