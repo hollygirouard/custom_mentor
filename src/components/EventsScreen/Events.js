0ec0c351d00dae8ecdc1cc925ed5945bec25a66d
austintedwards <austintedwards@gmail.com>
2017-11-09 10:03:55 -0700

    in the beginnings of working with the calendar

--- src/components/EventsScreen/Events.js
+++ src/components/EventsScreen/Events.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Events extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
-    const user = this.props.currentUser;
     return (
-      <div>
-        <h1>Events Screen</h1>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite La Croix flavor:
+          <select  value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
