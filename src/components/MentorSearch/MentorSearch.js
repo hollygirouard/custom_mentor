68b3d960ba94afa5da59b039b4d59ea7ff9e61d3
KelvinSajere <yovwisajere@gmail.com>
2017-11-27 04:20:05 -0700

    React-select Design of Search Filters

--- src/components/MentorSearch/MentorSearch.js
+++ src/components/MentorSearch/MentorSearch.js
@@ -1,78 +1,107 @@
-import React, { Component } from 'react';
-import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
+import React, {PropTypes, Component} from 'react';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
-export default class Steps extends Component {
-  render() {
-    return (
-      <div className="screen">
+const goals = [
+    { label: 'Education', value: 'Education' },
+    { label: 'Finiancial', value: 'Finiancial' },
+    { label: 'Spiritual', value: 'Spiritual' },
+    { label: 'Physical(Health)', value: 'Physical' },
 
-        <h3>Mentor Search</h3>
-        <Form>
-          <Col sm={2}>
-            <FormGroup>
-              <Label for="goals">Goals</Label>
-              <Input type="select" name="goals" id="goals" multiple>
-                <option>Education</option>
-                <option>Finaincial</option>
-                <option>Physical(Health)</option>
-                <option>Spiritual</option>
-                <option>Other</option>
-              </Input>
-            </FormGroup>
-          </Col>
-          <Col sm={2}>
-            <FormGroup>
-              <Label for="mentorlevel">Mentoring Level</Label>
-              <Input type="select" name="mentorlevel" id="mentorlevel" multiple>
-                <option>Cheerleader</option>
-                <option>Mentor</option>
-                <option>Coach</option>
-                <option>Tutor</option>
+];
+const contact = [
+    { label: 'Email', value: 'Email' },
+    { label: 'Phone', value: 'Phone' },
+    { label: 'SMS', value: 'SMS' },
 
-              </Input>
-            </FormGroup>
-          </Col>
-          <Col sm={2}>
-            <FormGroup>
-              <Label for="contact">Contact Method</Label>
-              <Input type="select" name="contact" id="contact" multiple>
-                <option>Email</option>
-                <option>Phone</option>
-                <option>Text</option>
 
-              </Input>
-            </FormGroup>
-          </Col>
-          <Col sm={2}>
-            <FormGroup>
-              <Label for="availablility">Availablility</Label>
-              <Input type="select" name="availablility" id="availablility" multiple>
-                <option>Monday</option>
-                <option>Tuesday</option>
-                <option>Wednesday</option>
-                <option>Thursday</option>
-                <option>Friday</option>
-                <option>Saturday</option>
-                <option>Sunday</option>
-              </Input>
-            </FormGroup>
-          </Col>
-          <Col sm={2}>
-            <FormGroup>
-              <Label for="edulevel">Educational Level</Label>
-              <Input type="select" name="edulevel" id="edulevel" multiple>
-                <option>High School Student</option>
-                <option>High School Graduate</option>
-                <option>College Student</option>
-                <option>Associate Degree</option>
-                <option>Bachelors Degree</option>
-                <option>Masters</option>
-                <option>P.H.D</option>
-              </Input>
-            </FormGroup>
-          </Col>
-        </Form>
-      </div>
-    );
+];
+const mentorlevel = [
+    { label: 'Cheer Leader', value: 'Cheerleader' },
+    { label: 'Mentor', value: 'Mentor' },
+    { label: 'Coach', value: 'Coach' },
+
+
+];
+const availability = [
+    { label: 'Monday', value: 'Monday' },
+    { label: 'Tuesday', value: 'Tuesday' },
+    { label: 'Wednesday', value: 'Wednesday' },
+    { label: 'Thursday', value: 'Thursday' },
+    { label: 'Friday', value: 'Friday' },
+    { label: 'Saturday', value: 'Saturday' },
+    { label: 'Sunday', value: 'Sunday' },
+
+
+];
+const edulevel = [
+    { label: 'High School Student', value: 'High School Student' },
+    { label: 'High School Graduate', value: 'High School Graduate' },
+    { label: 'College Student', value: 'College Student' },
+    { label: 'Associate Degree', value: 'Associate Degree' },
+    { label: 'Bachelors', value: 'Bachelors' },
+    { label: 'Masters', value: 'Masters' },
+    { label: 'P.H.D', value: 'P.H.D' },
+
+
+];
+
+export default class MentorSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      crazy: false,
+      contact: [],
+      goals:[],
+    };
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleSelectChange2 = this.handleSelectChange2.bind(this);
   }
+
+  handleSelectChange(value) {
+    console.log(this.target);
+    this.setState({ goals:value });
+  }
+  handleSelectChange2(value) {
+    console.log(this.target);
+    this.setState({ contact:value });
+  }
+
+  static propTypes = {
+    label: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.array,
+      PropTypes.object,
+    ])
+  }
+
+  render () {
+        return (
+            <div className="screen">
+                <h2>Mentor Search</h2>
+              <Form>
+                 <Col sm={2}>
+                <Select multi simpleValue value={this.state.goals} placeholder=" Goals" options={goals} onChange={this.handleSelectChange} />
+                </Col>
+                <Col sm={2}>
+               <Select  simpleValue value={this.state.mentorlevel} placeholder="Mentoring Level" options={mentorlevel} onChange={this.handleSelectChange2} />
+               </Col>
+               <Col sm={2}>
+              <Select multi simpleValue value={this.state.contact} placeholder=" Contact" options={contact} onChange={this.handleSelectChange} />
+              </Col>
+              <Col sm={2}>
+             <Select multi simpleValue value={this.state.availability} placeholder=" Availability" options={availability} onChange={this.handleSelectChange} />
+             </Col>
+             <Col sm={2}>
+            <Select  simpleValue value={this.state.edulevel} placeholder=" Educational Level" options={edulevel} onChange={this.handleSelectChange} />
+            </Col>
+            <Col sm={2}>
+ <Button color="primary" size="sm">Search</Button>{' '}
+           </Col>
+         </Form>
+
+            </div>
+        );
+    }
 }
