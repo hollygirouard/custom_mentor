1b5ed7f4a250c105a6dd36e063930d819c95bcfa
KelvinSajere <yovwisajere@gmail.com>
2017-11-27 05:32:24 -0700

    Complete Search Form Design

--- src/components/MentorSearch/MentorSearch.js
+++ src/components/MentorSearch/MentorSearch.js
@@ -1,6 +1,8 @@
 import React, {PropTypes, Component} from 'react';
 import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import Select from 'react-select';
+
+import {ReactSelectize, SimpleSelect, MultiSelect} from 'react-selectize';
 import 'react-select/dist/react-select.css';
 
 const goals = [
@@ -53,19 +55,44 @@ export default class MentorSearch extends Component {
     this.state = {
       crazy: false,
       contact: [],
+      mentorlevel:'',
       goals:[],
+      availability:[],
+      edulevel:'',
     };
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-    this.handleSelectChange2 = this.handleSelectChange2.bind(this);
+    this.handle_goals = this.handle_goals.bind(this);
+    this.handle_mentor_level = this.handle_mentor_level.bind(this);
+    this.handle_contact = this.handle_contact.bind(this);
+    this.handle_availaiblity = this.handle_availaiblity.bind(this);
+    this.handle_edu = this.handle_edu.bind(this);
+    this.handle_submit = this.handle_submit.bind(this);
+
   }
 
-  handleSelectChange(value) {
-    console.log(this.target);
+  handle_goals(value) {
+
     this.setState({ goals:value });
   }
-  handleSelectChange2(value) {
-    console.log(this.target);
+  handle_mentor_level(value) {
+    console.log(this.state);
+    this.setState({ mentorlevel:value });
+  }
+  handle_contact(value) {
+    console.log(this.state);
     this.setState({ contact:value });
+  }
+  handle_availaiblity(value) {
+    console.log(this.state);
+    this.setState({ availability:value });
+  }
+  handle_edu(value) {
+    console.log(this.state);
+    this.setState({ edulevel:value });
+  }
+  handle_submit(event){
+    event.preventDefault();
+    console.log(this.state);
+
   }
 
   static propTypes = {
@@ -80,24 +107,24 @@ export default class MentorSearch extends Component {
         return (
             <div className="screen">
                 <h2>Mentor Search</h2>
-              <Form>
+              <Form onSubmit={this.handle_submit}>
                  <Col sm={2}>
-                <Select multi simpleValue value={this.state.goals} placeholder=" Goals" options={goals} onChange={this.handleSelectChange} />
+                   <Select multi simpleValue value={this.state.goals} placeholder="Goals " options={goals} onChange={this.handle_goals} />
                 </Col>
                 <Col sm={2}>
-               <Select  simpleValue value={this.state.mentorlevel} placeholder="Mentoring Level" options={mentorlevel} onChange={this.handleSelectChange2} />
+               <Select simpleValue value={this.state.mentorlevel} placeholder="Mentoring Level" options={mentorlevel} onChange={this.handle_mentor_level} />
                </Col>
                <Col sm={2}>
-              <Select multi simpleValue value={this.state.contact} placeholder=" Contact" options={contact} onChange={this.handleSelectChange} />
+              <Select multi simpleValue value={this.state.contact} placeholder=" Contact" options={contact} onChange={this.handle_contact} />
               </Col>
               <Col sm={2}>
-             <Select multi simpleValue value={this.state.availability} placeholder=" Availability" options={availability} onChange={this.handleSelectChange} />
+             <Select multi simpleValue value={this.state.availability} placeholder=" Availability" options={availability} onChange={this.handle_availaiblity} />
              </Col>
              <Col sm={2}>
-            <Select  simpleValue value={this.state.edulevel} placeholder=" Educational Level" options={edulevel} onChange={this.handleSelectChange} />
+            <Select  simpleValue value={this.state.edulevel} placeholder=" Educational Level" options={edulevel} onChange={this.handle_edu} />
             </Col>
             <Col sm={2}>
- <Button color="primary" size="sm">Search</Button>{' '}
+                <Button color="primary" size="sm">Search</Button>{' '}
            </Col>
          </Form>
 
